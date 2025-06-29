4670c684a1b30dc732fbfa6a4e2b07121c964bb8
Stephen Ekeh <stevenadibee@yahoo.com>
2025-06-29 18:44:03 +0000

    Use environment variables in application.yml and SpringJwtApplication.java

--- src/main/java/com/hellosteve/springjwt/SpringJwtApplication.java
+++ src/main/java/com/hellosteve/springjwt/SpringJwtApplication.java
@@ -8,13 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJwtApplication {
 
 	public static void main(String[] args) {
-		// Load .env file manually
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 
 		SpringApplication.run(SpringJwtApplication.class, args);
 	}
